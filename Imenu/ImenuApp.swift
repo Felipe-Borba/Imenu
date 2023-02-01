5b7e446520adaf5baedbbd987830adef6964ba66
Felipe Borba <felipe.borba0109@gmail.com>
2023-01-31 21:39:10 -0300

    Initial Commit

---
+++ Imenu/ImenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImenuApp.swift
+//  Imenu
+//
+//  Created by Felipe Silva de Borba on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ImenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
