82613a021b07ea020132e44ea48620a71e93293f
Felipe Borba <felipe.borba0109@gmail.com>
2023-02-01 21:28:39 -0300

    add item row view

---
+++ Imenu/ItemRow.swift
@@ -0,0 +1,29 @@
+//
+//  ItemRow.swift
+//  Imenu
+//
+//  Created by Felipe Silva de Borba on 01/02/23.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    let item: MenuItem
+    
+    var body: some View {
+        HStack {
+            Image(item.thumbnailImage)
+            
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text("$\(item.price)")
+            }
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
