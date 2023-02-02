2dd89d676087df5c028c3d3d4c0174144eb98ae6
Felipe Borba <felipe.borba0109@gmail.com>
2023-02-01 22:22:14 -0300

    add a better navigation, add item detail screen

---
+++ Imenu/ItemDetail.swift
@@ -0,0 +1,44 @@
+//
+//  ItemDetail.swift
+//  Imenu
+//
+//  Created by Felipe Silva de Borba on 01/02/23.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    let item: MenuItem
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottomTrailing) {
+                Image(item.mainImage)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("Photo: \(item.photoCredit)")
+                    .padding(4)
+                    .background(.black)
+                    .font(.caption)
+                    .foregroundColor(.white)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(item.description)
+                .padding()
+            
+            Spacer()
+        }
+        .navigationTitle(item.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct ItemDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            ItemDetail(item: MenuItem.example)
+        }
+    }
+}
