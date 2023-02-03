e331728ebc1987a621825b7a2b222ce63db19662
Felipe Borba <felipe.borba0109@gmail.com>
2023-02-02 21:22:32 -0300

    change order to an observable object and make it an environment object

--- Imenu/ImenuApp.swift
+++ Imenu/ImenuApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct ImenuApp: App {
+    @StateObject var order = Order()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(order)
         }
     }
 }
