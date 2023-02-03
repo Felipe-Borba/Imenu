7061e8803b6ff8a85b5ea9cbdd623d6f0b07708d
Felipe Borba <felipe.borba0109@gmail.com>
2023-02-03 20:12:34 -0300

    Adding swipe to delete and EditButton

--- Imenu/OrderView.swift
+++ Imenu/OrderView.swift
@@ -22,6 +22,7 @@ struct OrderView: View {
                             Text("$\(item.price)")
                         }
                     }
+                    .onDelete(perform: deleteItems)
                 }
                 
                 Section {
@@ -29,9 +30,17 @@ struct OrderView: View {
                         CheckoutView()
                     }
                 }
+                .disabled(order.items.isEmpty)
             }
             .navigationTitle("Order")
+            .toolbar {
+                EditButton()
+            }
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        order.items.remove(atOffsets: offsets)
     }
 }
 
